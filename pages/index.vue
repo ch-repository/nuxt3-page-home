cbdd7d572b77ca1bff19218491f02e3cedc91e64
yaochaohang <yaochaohang219@gmail.com>
2023-07-25 16:47:43 +0800

    添加页脚

--- pages/index.vue
+++ pages/index.vue
@@ -224,7 +224,8 @@
               </p>
 
               <p class="mb-6 text-gray-500 sm:text-lg mt-6 leading-6">
-                主修课程：HTML5、CSS3、JavaScript、Jquery、Photoshop、C 语言、PHP、Python、计算机网络、计算机应用、Mysql
+                主修课程：HTML5、CSS3、JavaScript、Jquery、Photoshop、C
+                语言、PHP、Python、计算机网络、计算机应用、Mysql
               </p>
 
               <p class="mb-6 text-gray-500 sm:text-lg md:mb-8 leading-6">
@@ -237,8 +238,140 @@
       </div>
     </div>
 
-    <!-- 临时占位 -->
-    <div class="h-[300px]"></div>
+    <!-- 联系我 -->
+    <div class="w-full md:w-10/12 mx-auto px-6">
+      <h2
+        class="mt-12 md:mt-24 mb-6 text-center text-4xl text-slate-600 font-bold wow animate__fadeIn"
+      >
+        <span class="relative contact">CONTACT ME</span>
+      </h2>
+
+      <div class="bg-white py-6 sm:py-8 lg:py-12">
+        <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <div class="mb-10 md:mb-16 wow animate__fadeInUp">
+            <h2
+              class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl"
+            >
+              个人邮箱
+            </h2>
+
+            <p
+              class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg"
+            >
+              通过下列表单可以直接向作者发送邮件
+            </p>
+          </div>
+
+          <form
+            class="mx-auto grid max-w-screen-md gap-4 sm:grid-cols-2 wow animate__fadeInUp"
+          >
+            <div class="sm:col-span-2">
+              <label
+                for="name"
+                class="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+                >用户名*</label
+              >
+              <input
+                name="name"
+                class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+              />
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="email"
+                class="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+                >邮箱*</label
+              >
+              <input
+                name="email"
+                class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+              />
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="message"
+                class="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+                >内容*</label
+              >
+              <textarea
+                name="message"
+                class="h-64 w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+              ></textarea>
+            </div>
+
+            <div class="flex items-center justify-between sm:col-span-2">
+              <button
+                class="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
+              >
+                发送
+              </button>
+            </div>
+
+            <p class="text-xs text-gray-400">
+              想要学习前端相关技术也可以放我作者个人博客
+              <a
+                href="https://www.chaohangweb.cn"
+                target="_blank"
+                class="underline transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+                >像素脉动</a
+              >.
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <!-- 页脚 -->
+    <div class="pt-4 bg-slate-900">
+      <footer class="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div
+          class="flex flex-col items-center justify-between gap-4 border-t border-b md:flex-row"
+        >
+          <nav
+            class="flex flex-wrap justify-center gap-x-4 gap-y-2 md:justify-start md:gap-6"
+          >
+            <a
+              href="https://www.chaohangweb.cn"
+              target="_black"
+              class="text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600"
+              >像素脉动（Blog）</a
+            >
+          </nav>
+
+          <div class="flex gap-4">
+            <a
+              href="https://github.com/Yao-Chaohang"
+              target="_blank"
+              class="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600"
+            >
+              <svg
+                class="h-5 w-5"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        <div class="pt-2 text-center text-sm text-gray-400">
+          © 2023 - Present 姚超航. All rights reserved.
+        </div>
+        <div class="py-2 text-center text-sm text-gray-400">
+          <a href="https://beian.miit.gov.cn/" target="_blank"
+            >冀ICP备2023017514号-1</a
+          >
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -457,6 +590,18 @@ onUnmounted(() => {
 
   .educational::before {
     content: "EDUCATIONAL";
+    position: absolute;
+    top: -12px;
+    font-size: 48px;
+    color: #a5a5a5;
+    opacity: 0.2;
+    white-space: nowrap;
+    transform: translateX(-12%);
+    font-weight: bold;
+  }
+
+  .contact::before {
+    content: "CONTACT ME";
     position: absolute;
     top: -12px;
     font-size: 48px;
